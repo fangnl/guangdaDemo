af522da33f0a50b1ef804efd7b7b99fede80a4b6
fangnailiang <849834181@qq.com>
2020-09-04 18:47:28 +0800

    尝试

--- src/main/java/com/datah/main/App.java
+++ src/main/java/com/datah/main/App.java
@@ -1,12 +1,7 @@
 package com.datah.main;
 
 import com.datah.util.MergeException;
-import com.datah.util.MergeFileConstant;
 import com.datah.util.MergeUtil;
-import com.datah.util.PropertiesUtil;
-
-import java.util.MissingResourceException;
-
 public class App {
     public static void main(String[] args) throws MergeException {
         //读取配置文件的路径
